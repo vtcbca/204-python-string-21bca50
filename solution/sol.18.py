54f5a4ce2ca48480a0cc494a002da9c3a0a971e5
21bca50 <96609131+21bca50@users.noreply.github.com>
2022-03-25 11:07:26 +0530

    Update sol.18.py

--- solution/sol.18.py
+++ solution/sol.18.py
@@ -1,21 +1,15 @@
-#was to enter 5 string in list & check print string has even number
-x=[]
+l=[]
 count=0
-def createlist(x):
-    x=[]
-
+def createList(l):
     for i in range(5):
-        b=input("Enter any string:")
-        x.append(b)
-createlist(x)
-for i in x:
-    count=0
-    for j in i:
-        count=count+1
-    if(count%2==0):
-        print('The list is {} and its length is {}'.format(i,count))
-
-
-
-
-     
+        s=input('Enter any string : ')
+        l.append(s)
+createList(l)
+def evenlength(l):
+    for i in l:
+        count=0
+        for j in i:
+            count=count+1
+        if(count%2==0):
+            print('The list is {} and its length is {}.'.format(i,count))
+evenlength(l)
